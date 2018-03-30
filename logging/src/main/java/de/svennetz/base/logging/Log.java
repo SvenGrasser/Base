f399caac6d2691157f3564f85977b3fe2b16b70b
Sven Grasser <sven.grasser@web.de>
2018-03-30 12:56:27 +0200

    inital commit

---
+++ logging/src/main/java/de/svennetz/base/logging/Log.java
@@ -0,0 +1,21 @@
+package de.svennetz.base.logging;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class Log {
+	
+	private final Logger logger;
+	
+	public Log(Class<?> c){
+		logger = LogManager.getLogger(c);
+		BasicConfigurator.configure();
+	}
+	
+	public void info(String message) {
+		String logMessage = String.format("%s=%s %s=%s", "application", "tippspiel", "message", message);
+		logger.info(logMessage);
+	}
+}
+
