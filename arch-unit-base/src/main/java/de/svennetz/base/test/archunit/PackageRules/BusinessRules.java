b660f91f5f877f4c1383389930d6438f25ca5212
Sven Grasser <sven.grasser@web.de>
2018-08-24 19:12:51 +0200

    test

--- arch-unit-base/src/main/java/de/svennetz/base/test/archunit/PackageRules/BusinessRules.java
+++ arch-unit-base/src/main/java/de/svennetz/base/test/archunit/PackageRules/BusinessRules.java
@@ -1,13 +1,13 @@
 package de.svennetz.base.test.archunit.PackageRules;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
 public class BusinessRules {
 	@ArchTest
-	public static final ArchRule business_should_not_call_services = noClasses()
+	public static final ArchRule business_should_not_call_services = classes()
     .that().resideInAPackage("..business..")
     .should().accessClassesThat().resideInAPackage("..services..");
 
