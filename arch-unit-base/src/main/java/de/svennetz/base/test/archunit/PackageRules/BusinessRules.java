bd787edaac3c0ca47f409e3818b39d0f24a41d6b
Sven Grasser <sven.grasser@web.de>
2018-08-24 10:53:38 +0200

    Erste Version arch-unit-base jar

---
+++ arch-unit-base/src/main/java/de/svennetz/base/test/archunit/PackageRules/BusinessRules.java
@@ -0,0 +1,14 @@
+package de.svennetz.base.test.archunit.PackageRules;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+public class BusinessRules {
+	@ArchTest
+	public static final ArchRule business_should_not_call_services = noClasses()
+    .that().resideInAPackage("..business..")
+    .should().accessClassesThat().resideInAPackage("..services..");
+
+}
---
+++ arch-unit-base/src/main/java/de/svennetz/base/test/archunit/PackageRules/RepositoryRules.java
@@ -0,0 +1,18 @@
+package de.svennetz.base.test.archunit.PackageRules;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+public class RepositoryRules {
+	@ArchTest
+	public static final ArchRule repository_should_not_call_business = noClasses()
+    .that().resideInAPackage("..repositories..")
+    .should().accessClassesThat().resideInAPackage("..business..");
+	
+	@ArchTest
+	public static final ArchRule repository_should_not_call_services = noClasses()
+    .that().resideInAPackage("..repositories..")
+    .should().accessClassesThat().resideInAPackage("..services..");
+}
